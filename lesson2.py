870c0ccdf884073981dc4116f901580b85a1344b
Alina A <antolina94@gmail.com>
2024-05-19 23:19:10 +0300

    Initial commit

---
+++ lesson2.py
@@ -0,0 +1,7 @@
+completed_HW = 12
+hours_total = 1.5
+course = 'Python'
+time_per_exrs = hours_total / completed_HW
+
+print("Курс: " + course + ", всего задач:", str(completed_HW) + ", затрачено часов: ", str(hours_total) +
+      ", среднее время выполнения: ", time_per_exrs, "часа.")
